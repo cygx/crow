979ac9d4a4a7fe85c5c4e47195e7f6cb5dbca0c9
cygx <cygx@cpan.org>
2016-11-12 01:54:24 +0100

    respect PRINT_STACK_TRACE env var when testing and properly restore stdout

--- sources/Test.java
+++ sources/Test.java
@@ -5,6 +5,10 @@ import static java.util.Arrays.sort;
 
 public interface Test {
     static void run(Class c) {
+        run(c, "1".equals(System.getenv("PRINT_STACK_TRACE")));
+    }
+
+    static void run(Class c, boolean printStackTrace) {
         Method[] methods = c.getDeclaredMethods() ;
         sort(methods, (a, b) -> a.getName().compareTo(b.getName()));
 
@@ -17,45 +21,55 @@ public interface Test {
         System.out.println("1.." + count);
 
         final PrintStream out = System.out;
-        System.setOut(new PrintStream(new OutputStream() {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            public void write(int b) throws IOException {
-                if(b != '\n') bos.write(b);
-                else {
+        try {
+            System.setOut(new PrintStream(new OutputStream() {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                public void write(int b) throws IOException {
+                    if(b != '\n') bos.write(b);
+                    else {
+                        byte[] bytes = bos.toByteArray();
+                        bos.reset();
+                        out.write(' ');
+                        out.write(bytes);
+                        out.println();
+                    }
+                }
+                public void flush() throws IOException {
                     byte[] bytes = bos.toByteArray();
                     bos.reset();
-                    out.write(' ');
                     out.write(bytes);
-                    out.println();
+                    out.flush();
+                }
+            }));
+
+            int id = 0;
+            for(Method m : methods) {
+                if(!m.getName().startsWith("_")) continue;
+                String ok = "ok " + ++id + " -" + m.getName().replace('_', ' ');
+
+                try {
+                    m.invoke(null);
+                    out.println(ok);
+                }
+                catch(IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+                catch(InvocationTargetException e) {
+                    Throwable cause = e.getCause();
+                    String msg = cause.getMessage();
+                    if(msg != null) msg = msg.replace("\n", "\n  ");
+                    out.printf(" %s: %s\n",
+                        cause.getClass().getSimpleName(), msg);
+
+                    if(printStackTrace) {
+                        for(StackTraceElement s : cause.getStackTrace())
+                            out.println("   at " + s);
+                    }
+
+                    out.println("not " + ok);
                 }
             }
-            public void flush() throws IOException {
-                byte[] bytes = bos.toByteArray();
-                bos.reset();
-                out.write(bytes);
-                out.flush();
-            }
-        }));
-
-        int id = 0;
-        for(Method m : methods) {
-            if(!m.getName().startsWith("_")) continue;
-            String ok = "ok " + ++id + " -" + m.getName().replace('_', ' ');
-
-            try {
-                m.invoke(null);
-                out.println(ok);
-            }
-            catch(IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
-            catch(InvocationTargetException e) {
-                Throwable cause = e.getCause();
-                String msg = cause.getMessage();
-                if(msg != null) msg = msg.replace("\n", "\n  ");
-                out.printf(" %s: %s\n", cause.getClass().getSimpleName(), msg);
-                out.println("not " + ok);
-            }
         }
+        finally { System.setOut(out); }
     }
 }
